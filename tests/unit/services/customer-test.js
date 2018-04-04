43fb07a63bbb6e3bc2b68ffc68fc3201d2e00a94
The Game <chris.game@robbiebobbins.com>
2018-04-04 17:46:37 +0100

    unhappy scenarios for customer service

--- tests/unit/services/customer-test.js
+++ tests/unit/services/customer-test.js
@@ -1,18 +1,24 @@
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
 module('Unit | Service | customer', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('customer list it returned', function(assert) {
-    let customer = this.owner.lookup('service:customer');
-    customer.fetchAll()
-      .then(customerList => assert.equal(
-        customerList.length,
-        32,
-        'the list contains the expected number of entries'
-      ))
+  module('list is returned', function() {
+    test('when source text is json objects separated by new lines', function(assert) {
+      let customer = this.owner.lookup('service:customer');
+      customer.fetchAll()
+        .then(customerList => assert.equal(
+          customerList.length,
+          32,
+          'the list contains the expected number of entries'
+        ))
+    });
+  });
+
+  module('flash message is sent when list cannot be fetched', function() {
+    skip('because of network issues');
+    skip('because of a parsing error');
   });
 });
 
