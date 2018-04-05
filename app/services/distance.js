e3d7032412c07daecc54b2d567b2aa95d5d0d12b
The Game <chris.game@robbiebobbins.com>
2018-04-05 17:44:31 +0100

    implementation of distance service happy path
    I read the wikipedia article on Great-circle distance and noticed it
    stated that the haversine formula is better over small distances. Having
    read this i looked for an existing implementation of the haversine
    formula, i hope this doesn't mean i have skipped an important part of
    the exercise! The notes i have been given do also state that the code
    should be submitted as in a production shippable state. It was with this
    in mind that i chose an existing well maintained implementation of the
    formula. This would always be preference where existing packages meet
    the requirements because of the time savings of not reinventing the wheel
    and having had a larger audience to contribute to the solution.
    
    I selected the package with the most stars and made sure that it had
    multiple committers and the fact that it has been around since 2013 and
    has been committed to recently made me more confident.
    
    I have included a couple of sanity checks in the test just to make extra
    sure the implementation is sound.

--- app/services/distance.js
+++ app/services/distance.js
@@ -1,4 +1,9 @@
 import Service from '@ember/service';
+import haversine from 'npm:haversine';
 
 export default Service.extend({
+
+  between(a, b) {
+    return haversine(a,b);
+  }
 });
--- tests/unit/services/distance-test.js
+++ tests/unit/services/distance-test.js
@@ -10,8 +10,14 @@ module('Unit | Service | distance', function(hooks) {
     let guinnessStorehouse = { latitude: 53.342233, longitude: 6.286738 };
     let drumraneyChurch = { latitude: 53.481747, longitude: 7.749939 };
 
-    assert.equal(distance.between(postOffice, guinnessStorehouse), '1.92', 'the distance between Dublin post office and the Guinness Storehouse is the same as google maps reports');
-    assert.equal(distance.between(postOffice, drumraneyChurch), '99.74', 'the distance between Dublin post office and Drumraney Church is the same as google maps reports');
+    assert.equal(
+      Math.round(distance.between(postOffice, guinnessStorehouse)),
+      Math.round(1.92),
+      'the distance between Dublin post office and the Guinness Storehouse is the same as google maps reports');
+    assert.equal(
+      Math.round(distance.between(postOffice, drumraneyChurch)),
+      Math.round(99.74),
+      'the distance between Dublin post office and Drumraney Church is the same as google maps reports');
   });
 });
 
