23daab22ed0ebc0778d5f6b391524e2b83ed3d85
The Game <chris.game@robbiebobbins.com>
2018-04-05 16:06:56 +0100

    handle all unhappy scenarios
    I have used mirage to mock out the interaction with the file url, this
    allows me to simulate error scenarios in the tests.
    
    I made the service return flash messages if there are any errors.
    This makes the service responsible for dealing with anything regarding
    fetching and parsing of the customer list allowing these steps to be
    tested fully in one place.

--- app/services/customer.js
+++ app/services/customer.js
@@ -1,11 +1,33 @@
-import Service from '@ember/service';
+import Service, { inject as service } from '@ember/service';
 import fetch from 'fetch';
 import ENV from 'customer-food-and-drinks/config/environment';
 
 export default Service.extend({
+  flashMessages: service(),
+
   fetchAll() {
+    let flashMessages = this.get('flashMessages');
+
     return fetch(ENV.customerURL)
-      .then(response => response.text())
-      .then(text => JSON.parse(`[${text.split('\n').join(',')}]`));
+      .then(response => {
+        if (response.ok) {
+          return response.text().then(text => {
+            try {
+              return JSON.parse(`[${text.split('\n').join(',')}]`);
+            }
+            catch(error) {
+              throw new Error(
+                `The customer list could not be parsed because of a formatting error:\n ${error.message}`
+              );
+            }
+          });
+        }
+        throw new Error(
+          'The customer list could not be fetched because of a network error, please try again'
+        );
+      })
+      .catch(error => {
+        flashMessages.alert(error.message);
+      });
   }
 });
--- tests/unit/services/customer-test.js
+++ tests/unit/services/customer-test.js
@@ -1,24 +1,79 @@
-import { module, test, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { startMirage } from 'customer-food-and-drinks/initializers/ember-cli-mirage';
+import ENV from 'customer-food-and-drinks/config/environment';
 
 module('Unit | Service | customer', function(hooks) {
   setupTest(hooks);
 
+  let customer;
+
+  hooks.beforeEach(function() {
+    this.server = startMirage();
+    this.server.logging = true;
+    customer = this.owner.lookup('service:customer');
+  });
+
+  hooks.afterEach(function() {
+    this.server.shutdown();
+  });
+
   module('list is returned', function() {
     test('when source text is json objects separated by new lines', function(assert) {
-      let customer = this.owner.lookup('service:customer');
+      server.get(ENV.customerURL, () => {
+        return `
+          {"latitude": "1.1", "user_id": 1, "name": "Dave", "longitude": "-1.1"}
+          {"latitude": "1.2", "user_id": 2, "name": "Bob", "longitude": "-2.2"}
+        `.trim();
+      });
+
       customer.fetchAll()
         .then(customerList => assert.equal(
           customerList.length,
-          32,
+          2,
           'the list contains the expected number of entries'
         ))
     });
   });
 
-  module('flash message is sent when list cannot be fetched', function() {
-    skip('because of network issues');
-    skip('because of a parsing error');
+  let flashMessages;
+
+  module('flash message is sent when list cannot be fetched', function(hooks) {
+    hooks.beforeEach(function() {
+      flashMessages = this.owner.lookup('service:flashMessages');
+    });
+
+    test('because of a network error', function(assert) {
+      server.get(ENV.customerURL, () => {}, 500);
+
+      customer.fetchAll()
+        .then(customerList => {
+          assert.notOk(customerList, 'no list is returned');
+          assert.equal(flashMessages.queue.length, 1, 'a flash message has been added');
+          assert.equal(
+            flashMessages.queue[0].message,
+            'The customer list could not be fetched because of a network error, please try again',
+            'a flash message has been added about a network error'
+          );
+        });
+    });
+
+    test('because of a parsing error', function(assert) {
+      server.get(ENV.customerURL, () => {
+        return '{';
+      });
+
+      customer.fetchAll()
+        .then(customerList => {
+          assert.notOk(customerList, 'no list is returned');
+          assert.equal(flashMessages.queue.length, 1, 'a flash message has been added');
+          assert.equal(
+            flashMessages.queue[0].message,
+            'The customer list could not be parsed because of a formatting error:\n Unexpected token ] in JSON at position 2',
+            'a flash message has been added about a parsing error'
+          );
+        });
+    });
   });
 });
 
