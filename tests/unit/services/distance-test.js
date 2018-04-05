4551b8613a60638a211f4ba07d8d5fde0cd33464
The Game <chris.game@robbiebobbins.com>
2018-04-05 17:13:42 +0100

    failing test for distance service happy path

---
+++ tests/unit/services/distance-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | distance', function(hooks) {
+  setupTest(hooks);
+
+  test('the distance between two points is calculated accurately', function(assert) {
+    let distance = this.owner.lookup('service:distance');
+    let postOffice = { latitude: 53.350047, longitude: 6.260689 };
+    let guinnessStorehouse = { latitude: 53.342233, longitude: 6.286738 };
+    let drumraneyChurch = { latitude: 53.481747, longitude: 7.749939 };
+
+    assert.equal(distance.between(postOffice, guinnessStorehouse), '1.92', 'the distance between Dublin post office and the Guinness Storehouse is the same as google maps reports');
+    assert.equal(distance.between(postOffice, drumraneyChurch), '99.74', 'the distance between Dublin post office and Drumraney Church is the same as google maps reports');
+  });
+});
+
